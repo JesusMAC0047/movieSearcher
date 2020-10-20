c48b0da315d60b04fb183f82381ff3671c5b7ac7
jesusMAC0047 <jesus0047_mac@outlook.com>
2020-10-19 21:30:50 -0500

    se termino la alerta del buscador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { RecommendedComponent } from './components/recommended/recommended.compo
     BrowserModule,
     routing,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     appRoutingProviders
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-main',
@@ -9,13 +9,35 @@ import { FormBuilder } from '@angular/forms';
 export class MainComponent implements OnInit {
 
   public placeholder: string;
+  public inputSearch: string = "";
+  public alertMessage: any;
   
-  constructor() {
+  constructor( private configAlert:NgbAlertConfig) {
+  
+    configAlert.type = "warning";
+    configAlert.dismissible = true;
   
     this.placeholder = "Ejemplo: Batman";
+    this.alertMessage = {type: 'ATENCIÓN',
+                        message: 'Ingrese el nombre de alguna película en ingles'};
   }
 
   ngOnInit(): void {
   }
 
+  onSearch(formSearch){
+    console.log(formSearch.value);
+    let search = formSearch.value.inputSearch;
+    if (search == "") {
+      document.getElementById('alerta').style.display="block";
+      console.log(search);
+    }else{
+      console.log(this.inputSearch);
+      document.getElementById('alerta').style.display="none";
+    }
+  }
+
+  close(alerta){
+    document.getElementById(alerta).style.display="none";
+  }
 }
